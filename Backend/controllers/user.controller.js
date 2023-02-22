0ffb04ebed293fb93ddd8a980c423dcdeeb3e07f
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-02-23 03:58:15 +0530

    user controller

---
+++ Backend/controllers/user.controller.js
@@ -0,0 +1,69 @@
+require("dotenv").config();
+const { UserModel } = require("../model/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+exports.signup = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        console.log(req.body);
+        const isPresent = await UserModel.find({ email });
+
+        if (isPresent.length === 0) {
+            // encrypte password and register
+            bcrypt.hash(password, 5, async (err, hash) => {
+                if (err) res.status(401).json({ "errow ": err.message });
+                else {
+                    const newUser = new UserModel({
+                        name,
+                        email,
+                        password: hash,
+                    });
+                    await newUser.save();
+                    res.status(200).json({ success: "user registered successfully" });
+                }
+            });
+        } else {
+            res.status(404).json({ msg: "user already registered" });
+        }
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const UserData = await UserModel.findOne({ email });
+
+        if (!UserData) {
+            res.status(404).json({ message: "user not found" });
+        }
+        // hash password form UserData(db.users)
+        const hashPassword = UserData?.password;
+
+        // compare
+
+        bcrypt.compare(password, hashPassword, (err, result) => {
+            if (result) {
+                // generate tokens 
+                const Normal_Token = jwt.sign({ userId: UserData._id }, process.env.Normal_Token_key, { expiresIn: "7d" })
+                const Refresh_Token = jwt.sign({ userId: UserData._id }, process.env.Refresh_Token_key, { expiresIn: "7d" })
+
+                // send token in cookies
+                res.cookie("Normal_Token", Normal_Token, { httpOnly: true })
+                res.cookie("Refresh_Token", Refresh_Token, { httpOnly: true })
+                res.status(200).json({ "message": "Login successfully", Normal_Token, Refresh_Token })
+            }
+            else {
+                res.status(401).json({ "message": "error while login" });
+            } 
+        })
+
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+
+}
+
--- Backend/middlewares/authenticate.middleware.js
+++ Backend/middlewares/authenticate.middleware.js
@@ -1,36 +1,30 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
-const fs = require('fs');
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
-const authenticate = (req,res,next)=>{
-    try {
+const authenticate = (req, res, next) => {
+  try {
+    const Normal_Token = req.cookies.Normal_Token || "";
 
-        const Normal_Token = req.cookies.Normal_Token || ''
-        
-        // blacklisted usign reddis
-        
-        const blacklistedToken = JSON.parse(fs.readFileSync('./blacklist.json','utf-8'))
+    // blacklisted usign reddis
 
-        if(blacklistedToken.includes(Normal_Token)){
-            res.status(401).json({message:"please login again"});
+    const blacklistedToken = JSON.parse(
+      fs.readFileSync("./blacklist.json", "utf-8")
+    );
 
-        }else{
-
-            
-            jwt.verify(Normal_Token,process.env.Normal_Token_key,(err,decoded)=>{
-            if(err) res.status(err).json({message: err.message});
-            else{
-                next()
-            }
-            
-        })
-
-        
+    if (blacklistedToken.includes(Normal_Token)) {
+      res.status(401).json({ message: "please login again" });
+    } else {
+      jwt.verify(Normal_Token, process.env.Normal_Token_key, (err, decoded) => {
+        if (err) res.status(err).json({ message: err.message });
+        else {
+          next();
+        }
+      });
     }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
-    } catch (error) {
-        res.status(400).json({"message" : error.message});
-    }
-}
-
-module.exports = {authenticate}
+module.exports = { authenticate };
