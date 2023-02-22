a871e5c57da08c78d47ce2af48a7c249d5c001ca
Raushan <raushansharma7462@gmail.com>
2023-02-22 23:43:37 +0530

    ChatRouterSetup

--- Backend/routes/chatroute.js
+++ Backend/routes/chatroute.js
@@ -1,1 +1,16 @@
 // chatroutes
+const express = require("express")
+const chatRouter = express.Router()
+
+
+
+chatRouter.use(express.static("Frontend"));
+
+chatRouter.get('/', (req, res)=>{
+    res.send("CHAT ROUTE")
+})
+
+
+
+
+module.exports = {chatRouter}
