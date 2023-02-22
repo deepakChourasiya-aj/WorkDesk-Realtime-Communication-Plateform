cfebe643ae658def983044e4e6234c3ab13135c8
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-02-23 02:34:23 +0530

    created files

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,41 @@
-// index
+const express = require("express");
+const redis = require("redis");
+const app = express();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { dbConnection } = require("./config/db");
+const { userRouter } = require("./routes/user.route");
+const { authenticate } = require("./middlewares/authenticate.middleware");
+const { weatherRouter } = require("./routes/weather.route");
+const { LogoutRouter } = require("./routes/logout.route");
+
+// middleware
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req,res)=>res.send('home_Route'))
+
+// routers
+app.use('/user',userRouter)
+app.use(authenticate) //will validate login status
+app.use('/weather',weatherRouter)
+app.use('/logout',LogoutRouter)
+
+// generate new token
+app.get('/newToken',async(req,res) => {
+  const refreshToken = res.cookies.Rre
+})
+
+// server listens
+app.listen(8080, async () => {
+  try {
+    dbConnection;
+    console.log(`server listening on ${8080}`);
+  } catch (error) {
+    console.log(`error while connecting to ${error.message}`);
+  }
+});
+
