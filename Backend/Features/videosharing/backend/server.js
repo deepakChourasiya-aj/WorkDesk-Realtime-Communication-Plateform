68ab4cb12106636bff0c11cfdcf6f600c2955c57
owais azmi <owaisazmi@MacBook-Air.local>
2023-02-23 23:42:54 +0530

    Server Done

--- Backend/Features/videosharing/backend/server.js
+++ Backend/Features/videosharing/backend/server.js
@@ -1,12 +1,42 @@
-const express=require("express");
+// require("dotenv").config({ path: "/Users/owaisazmi/Desktop/melted-group-7444/Backend/.env" });
 require("dotenv").config();
-const app=express();
+const express = require('express');
+const {Server} = require('socket.io');
+const http = require('http');
+const cors = require('cors');
 
-app.get("/",(req,res)=>{
-    console.log("Working");
-    res.send("working")
+const app = express()
+app.use(cors({
+    origin : '*'
+}))
+
+const httpServer =  http.createServer(app)
+
+app.get("/start" , (req,res) => {
+    res.send("Welcome To WrokDesk Video Server")
 })
 
-app.listen(8080,()=>{
-    console.log("server is listning on 8080");
+httpServer.listen(process.env.port , () => {
+    console.log(`Server started at `+process.env.port);
+})
+
+const io = new Server(httpServer , {
+    cors : {
+        origin : '*'
+    }
+})
+
+io.on('connection' , (socket) => {
+
+    socket.on('join-room' , (RoomID , userID) => {
+        // const {RoomID,userID} = data
+        console.log(RoomID , userID);
+        socket.join(RoomID)
+        socket.to(RoomID).emit('user-join' , userID)
+
+        socket.on('disconnect' , () => {
+            socket.to(RoomID).emit('user-disconnected', userID)
+        })
+    })
+
 })
