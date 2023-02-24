0b3fe396407bdd5d4e77517b43f7cd418c66c4e0
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-02-24 14:58:04 +0530

    24 , 2.58

---
+++ Frontend/scripts/index.js
@@ -0,0 +1,10 @@
+let swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 10,
+    mousewheel: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
