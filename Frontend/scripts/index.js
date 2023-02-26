2f30bb3474e22731ffbf885b272e01018049d947
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-02-26 21:53:21 +0530

    done

--- Frontend/scripts/index.js
+++ Frontend/scripts/index.js
@@ -1,3 +1,9 @@
+
+import hideLoading from "../components/hideLoading.components.js";
+import loading from "../components/loading.components.js";
+
+loading()
+
 let swiper = new Swiper(".mySwiper", {
     direction: "vertical",
     slidesPerView: 1,
@@ -8,3 +14,24 @@ let swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+
+const userdata = JSON.parse(localStorage.getItem('userdata')) 
+console.log(userdata);
+if(userdata) {
+  
+  let hello = document.getElementById('hellouser')
+  hello.innerText = "Hey "+userdata.name;
+  hideLoading()
+}else{
+  loading()
+  window.location.href = "./routes/loginSignup/login.html"
+  loading()
+}
+
+// logo redirection
+const homelogo = document.getElementById('homelogo')
+homelogo.addEventListener('click',()=>{
+  loading()
+  window.location.href= "index.html"
+} )
